61e0a84d56e3c20d174bd7546f65ba70bfdf96ae
KaphAein <cherifafadaly@gmail.com>
2024-10-10 07:47:29 -0700

    commit

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''type-annotated function make_multiplier'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''function make_multiplier'''
+    return lambda x: x * multiplier
