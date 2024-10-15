a3db42149f8e768f2a43155860020368232517d1
KaphAein <cherifafadaly@gmail.com>
2024-10-15 08:55:55 -0700

    commit

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 '''Async Comprehension module'''
-import asyncio
-import random
-from typing import Generator
+from typing import List
+from importlib import import_module as using
+
 
 async_generator = using('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     '''Task 1 function'''
     return [num async for num in async_generator()]
