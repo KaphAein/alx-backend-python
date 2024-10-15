069a8ed84b7d7405384fd3ec74a557813629ea5a
KaphAein <cherifafadaly@gmail.com>
2024-10-15 08:12:21 -0700

    commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,8 +5,8 @@ import random
 from typing import Generator
 
 
-Async def async_generator():
+async def async_generator():
     '''Task 0 function'''
-    for _ in rnage(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Async Comprehension module'''
+import asyncio
+import random
+from typing import Generator
+
+
+Async def async_generator():
+    '''Task 0 function'''
+    for _ in rnage(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
